759418dffed4d69559f4653081b715e5768a9f06
AzureAD\Ana-MarijaBarisic <ana-marija@mediatorium.co>
2023-03-27 08:47:43 +0200

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import { Footer, Header } from "./components";
+import { Admin, Cart, Contact, Home, Orders } from "./pages";
 
 function App() {
-  return <div className="App">Ecommerce</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Admin } from "./Admin/Admin";
+export { default as Cart } from "./Cart/Cart";
+export { default as Contact } from "./Contact/Contact";
+export { default as Home } from "./Home/Home";
+export { default as Orders } from "./Orders/Orders";
