5f3edf4168b67fc92b17f3008a00204dcc579639
AzureAD\Ana-MarijaBarisic <ana-marija@mediatorium.co>
2023-03-28 13:19:42 +0200

    registration logic

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,13 +4,12 @@ import styles from "./Loader.module.scss";
 import loader from "../../assets/loader.gif";
 
 const Loader = () => {
-  return ReactDOM.createPortal(
+  return (
     <div className={styles.wrapper}>
       <div className={styles.loader}>
         <img src={loader} alt="Loading..." />
       </div>
-    </div>,
-    document.getElementById("loader")
+    </div>
   );
 };
 
--- src/pages/Login/RegistrationContent.jsx
+++ src/pages/Login/RegistrationContent.jsx
@@ -1,22 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Login.module.scss";
 import regImg from "../../assets/register.png";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import { Loader } from "../../components";
+import { useNavigate } from "react-router-dom";
 
 const RegistrationContent = ({ setCurrentScreen }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match!");
+    }
+
+    setLoading(true);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        setLoading(false);
+        toast.success("Registration successfull");
+        navigate("/");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+        setLoading(false);
+      });
+  };
   return (
     <section className={`container ${styles.auth}`}>
+      {loading && <Loader />}
       <div className={styles.form}>
         <h2>Register</h2>
-        <form>
-          <input type="email" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
-          <input type="password" placeholder="Confirm Password" required />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            required
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
           <button
             className="--btn --btn-primary --btn-block"
             style={{ marginBlock: "1rem" }}
           >
             Register
           </button>
+          <ToastContainer />
         </form>
         <p style={{ marginBlock: "1.5rem" }}>
           Already hav an account?{" "}
