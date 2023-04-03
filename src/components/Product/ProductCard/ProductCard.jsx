196c7a4bb2f13b42883e5ad465f04238468875d1
AzureAD\Ana-MarijaBarisic <abarisic@protonmail.com>
2023-04-03 14:38:02 +0200

    product card setup

--- src/components/Product/ProductCard/ProductCard.jsx
+++ src/components/Product/ProductCard/ProductCard.jsx
@@ -2,8 +2,43 @@ import React from "react";
 
 import styles from "./ProductCard.module.scss";
 
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+const ProductCard = ({
+  name,
+  category,
+  brand,
+  description,
+  price,
+  imgUrl,
+  grid,
+}) => {
+  const shortenText = (text, n) => {
+    if (text.length > n) {
+      const shortened = text.substring(0, n).concat("...");
+      return shortened;
+    }
+    return text;
+  };
+
+  return (
+    <div className={grid ? `${styles.grid}` : `${styles.list}`}>
+      <div className={styles.img}>
+        {" "}
+        <img src={imgUrl} alt={name} />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.details}>
+          <p>${price}</p>
+          <h4>{grid ? shortenText(name, 20) : shortenText(name, 40)}</h4>
+          {!grid && (
+            <p className={styles.description}>
+              {shortenText(description, 200)}
+            </p>
+          )}
+        </div>
+        <button className="--btn --btn-danger">Add to cart</button>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
