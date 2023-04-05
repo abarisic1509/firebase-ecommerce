e9f32f9fda59162c8670d53c299e4a55026ab4e7
AzureAD\Ana-MarijaBarisic <abarisic@protonmail.com>
2023-04-05 09:55:28 +0200

    Filters responsivity

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductNav from "./ProductNav/ProductNav";
 import ProductList from "./ProductList/ProductList";
 
 import styles from "./Product.module.scss";
 import { useSelector } from "react-redux";
+import { FaCogs } from "react-icons/fa";
 
 const Product = () => {
   const productsData = useSelector((state) => state.product.products);
+  const [showProductNav, setShowProductNav] = useState(false);
 
   return (
     <section id="products">
       <div className={`container ${styles.product}`}>
-        <aside className={styles.filter}>
+        <aside
+          className={
+            showProductNav
+              ? `${styles.filter} ${styles.show}`
+              : `${styles.filter}`
+          }
+        >
           <ProductNav productsData={productsData} />
         </aside>
         <div className={styles.content}>
           <ProductList productsData={productsData} />
+          <button
+            className={styles.icon}
+            onClick={() => setShowProductNav((prev) => !prev)}
+          >
+            <FaCogs color="orangered" size={22} />
+            <p>
+              <b>{showProductNav ? "Hide filters" : "Show filters"}</b>
+            </p>
+          </button>
         </div>
       </div>
     </section>
