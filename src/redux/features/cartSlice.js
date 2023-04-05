c6fd774e3c4452813e5dfb471e859fb790d12fd3
AzureAD\Ana-MarijaBarisic <abarisic@protonmail.com>
2023-04-05 11:14:57 +0200

    Cart slice setup

---
+++ src/redux/features/cartSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [],
+  cartTotalQuantity: 0,
+  cartTotalAmount: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {},
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
